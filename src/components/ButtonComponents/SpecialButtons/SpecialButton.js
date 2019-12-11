fdf68a9b13b5e3ef90f9860f5d7cdfe09226d2af
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-10 21:26:33 -0500

    finished styling

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SpecialButton = (props) => {
   return (
     <>
-<button>{props.specials}</button>
+<button className="special_button">{props.specials}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
